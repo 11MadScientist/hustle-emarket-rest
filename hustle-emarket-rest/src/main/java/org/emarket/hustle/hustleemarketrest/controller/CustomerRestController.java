6e1d574f85ef776f6de01a2156e661cbf1aa3941
Makise-K <felnovert.lee@gmail.com>
2021-11-25 22:28:46 +0800

    Bug on updating the Customer Data, not updating because password was missing, needed to get the password from the database to pin it out to the new customer data

--- hustle-emarket-rest/src/main/java/org/emarket/hustle/hustleemarketrest/controller/CustomerRestController.java
+++ hustle-emarket-rest/src/main/java/org/emarket/hustle/hustleemarketrest/controller/CustomerRestController.java
@@ -64,6 +64,16 @@ public class CustomerRestController
 	@PutMapping("/customers")
 	public Customer updateCustomer(@RequestBody Customer customer)
 	{
+		/*
+		 * because we do not pass the password to the client, we need to update
+		 * the password when the client wants to update their data.
+		 * we can do this by getting the data first then pinning the saved password
+		 * from the database to passed Customer data
+		 */
+		Customer dbCustomer = customerService.getCustomerById(customer.getId());
+
+		customer.setPassword(dbCustomer.getPassword());
+
 		if(customer.getCustomerAddress() != null)
 		{
 			for (CustomerAddress address : customer.getCustomerAddress())
@@ -71,9 +81,24 @@ public class CustomerRestController
 				address.setCustomer(customer);
 			}
 		}
+<<<<<<< Updated upstream
 		customerService.saveCustomer(customer);
 		customer.getCustomerAddress();
 		return customer;
+=======
+
+		try
+		{
+			customerService.saveCustomer(customer);
+			return customer;
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+			throw new UniqueErrorException("Customer [email, username] should be unique");
+		}
+
+>>>>>>> Stashed changes
 	}
 
 	@DeleteMapping("/customers/{id}")
@@ -82,4 +107,66 @@ public class CustomerRestController
 		customerService.deleteCustomerById(id);
 		return ("Deleted Customer with id - " + id);
 	}
+<<<<<<< Updated upstream
+=======
+
+	// login
+	@PostMapping("/customers/login")
+	public Customer loginCustomer(@RequestBody Customer customer)
+	{
+		try
+		{
+			Customer dbCustomer = customerService.loginCustomer(customer.getUsername());
+
+			boolean result = bcrypt.matches(customer.getPassword(), dbCustomer.getPassword());
+			log.info(result + "");
+			if(bcrypt.matches(customer.getPassword(), dbCustomer.getPassword()))
+			{
+				return dbCustomer;
+			}
+			throw new CustomerNotFoundException("Customer [Email, Password] does not match.");
+
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+			throw new CustomerNotFoundException("No Customer with Found");
+		}
+	}
+	/*
+	 * we need to put a custom mapping for changing of password
+	 * because there is no method that checks if the password has
+	 * already been hashed, or not. so in order to hash the new
+	 * password and not hash the previous password, we need to create
+	 * a new mapping
+	 */
+
+	@PutMapping("/customers/changePassword")
+	public Customer changePassword(Customer customer)
+	{
+		customer.setPassword(bcrypt.encode(customer.getPassword()));
+		log.info(customer.getPassword());
+
+		if(customer.getCustomerAddress() != null)
+		{
+			for (CustomerAddress address : customer.getCustomerAddress())
+			{
+				address.setCustomer(customer);
+			}
+		}
+
+		customer.getCustomerDetail().setCustomer(customer);
+
+		try
+		{
+			customerService.saveCustomer(customer);
+			return customer;
+		}
+		catch (Exception e)
+		{
+			throw new RuntimeException("Saving new Password Failed");
+		}
+	}
+
+>>>>>>> Stashed changes
 }
