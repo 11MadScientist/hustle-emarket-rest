3b0d6db464447424ae267a9acb8f0f6c12ace0b7
Makise-K <felnovert.lee@gmail.com>
2022-04-27 15:04:50 +0800

    feat: implementing email with template (not done)

--- hustle-emarket-rest/src/main/java/org/emarket/hustle/emarkethustle/service/EmailSenderService.java
+++ hustle-emarket-rest/src/main/java/org/emarket/hustle/emarkethustle/service/EmailSenderService.java
@@ -1,15 +1,35 @@
 package org.emarket.hustle.emarkethustle.service;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.Map;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+import org.springframework.ui.freemarker.FreeMarkerTemplateUtils;
+
+import freemarker.core.ParseException;
+import freemarker.template.Configuration;
+import freemarker.template.MalformedTemplateNameException;
+import freemarker.template.Template;
+import freemarker.template.TemplateException;
+import freemarker.template.TemplateNotFoundException;
 
 @Service
 public class EmailSenderService
 {
 	@Autowired
 	private JavaMailSender mailSender;
+
+	@Autowired
+	private Configuration configuration;
 
 	public void sendEmail(
 			String toEmail,
@@ -24,5 +44,36 @@ public class EmailSenderService
 
 		mailSender.send(message);
 		System.out.println("Message Sent Successfully");
+	}
+
+	public void sendEmailWithAttachment(
+			Map<String, Object> model,
+			String toEmail,
+			String subject,
+			String attachment) throws MessagingException, TemplateNotFoundException, MalformedTemplateNameException,
+			ParseException, IOException, TemplateException
+	{
+		MimeMessage mimeMessage = mailSender.createMimeMessage();
+		MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage,
+				MimeMessageHelper.MULTIPART_MODE_MIXED_RELATED);
+
+		try
+		{
+			FileSystemResource fileSystemResource = new FileSystemResource(new File(attachment));
+			mimeMessageHelper.addAttachment(fileSystemResource.getFilename(), fileSystemResource);
+		}
+		catch (Exception e)
+		{
+			throw new MessagingException();
+		}
+
+		Template template = configuration.getTemplate("email-template.ftl");
+		String html = FreeMarkerTemplateUtils.processTemplateIntoString(template, model);
+
+		mimeMessageHelper.setFrom("${spring.mail.username}");
+		mimeMessageHelper.setTo(toEmail);
+		mimeMessageHelper.setSubject(subject);
+		mimeMessageHelper.setText(html, true);
+		mailSender.send(mimeMessage);
 	}
 }
